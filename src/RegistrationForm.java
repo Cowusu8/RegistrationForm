847b7409114d1996df59d1d44a8cf049ec031dc9
Crystal <crystalagyeman23@gmail.com>
2023-02-10 12:48:17 -0600

    second commit

--- src/RegistrationForm.java
+++ src/RegistrationForm.java
@@ -37,6 +37,8 @@ public class RegistrationForm implements ActionListener {
 	RegistrationForm() {
 
 		createWindow();// calling method from constructor
+		setLocationAndSize();
+		addComponentsToFrame();
 	}
 
 	// Creating user-defined method
@@ -50,6 +52,46 @@ public class RegistrationForm implements ActionListener {
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
+	}
+
+	public void setLocationAndSize() {
+		// Setting Location and Size of Each Component
+		nameLabel.setBounds(20, 20, 40, 70);
+		genderLabel.setBounds(20, 70, 80, 70);
+		fatherNameLabel.setBounds(20, 120, 100, 70);
+		passwordLabel.setBounds(20, 170, 100, 70);
+		confirmPasswordLabel.setBounds(20, 220, 140, 70);
+		cityLabel.setBounds(20, 270, 100, 70);
+		emailLabel.setBounds(20, 320, 100, 70);
+		nameTextField.setBounds(180, 43, 165, 23);
+		genderComboBox.setBounds(180, 93, 165, 23);
+		fatherTextField.setBounds(180, 143, 165, 23);
+		passwordField.setBounds(180, 193, 165, 23);
+		confirmPasswordField.setBounds(180, 243, 165, 23);
+		cityTextField.setBounds(180, 293, 165, 23);
+		emailTextField.setBounds(180, 343, 165, 23);
+		registerButton.setBounds(70, 400, 100, 35);
+		resetButton.setBounds(220, 400, 100, 35);
+	}
+
+	public void addComponentsToFrame() {
+		// Adding components to Frame
+		frame.add(nameLabel);
+		frame.add(genderLabel);
+		frame.add(fatherNameLabel);
+		frame.add(passwordLabel);
+		frame.add(confirmPasswordLabel);
+		frame.add(cityLabel);
+		frame.add(emailLabel);
+		frame.add(nameTextField);
+		frame.add(genderComboBox);
+		frame.add(fatherTextField);
+		frame.add(passwordField);
+		frame.add(confirmPasswordField);
+		frame.add(cityTextField);
+		frame.add(emailTextField);
+		frame.add(registerButton);
+		frame.add(resetButton);
 	}
 
 	@Override
